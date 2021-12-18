ebe89b478b2c996faa3868dce7d625f994e674ce
alara <alaraayan@hotmail.com>
2021-12-18 10:56:36 +0000

    casing corrected

--- todos/models.py
+++ todos/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Todo(models.Model):
     todo_item = models.CharField(max_length=100)
-    is_Active = models.BooleanField(default=True)
-    is_Done = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_done = models.BooleanField(default=False)
     owner = models.ForeignKey(
         'jwt_auth.User',
         related_name='todos',
